386615a80b221c3594d689f7be9b7a4c83ddca2f
Paul Plotnikov <ppasha81@gmail.com>
2022-09-10 18:37:32 +0300

    Module

--- src/main/java/com/config/FlywayConfig.java
+++ src/main/java/com/config/FlywayConfig.java
@@ -3,9 +3,9 @@ package com.config;
 import org.flywaydb.core.Flyway;
 
 public class FlywayConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "root";
+    private static final String URL = "postgres://qaaclpdmmcwymt:cb92dfb540428f30e2a97ae59cc98800fdd8204c07359467d66d4b0b9e877c3e@ec2-52-200-5-135.compute-1.amazonaws.com:5432/deimk83oeleopg";
+    private static final String USER = "qaaclpdmmcwymt";
+    private static final String PASSWORD = "cb92dfb540428f30e2a97ae59cc98800fdd8204c07359467d66d4b0b9e877c3e";
     private static final String SCHEMA = "module";
     private static final String LOCATION = "db/migration";
 
