507d4685a9396dc5d9769d46b1f2609b964584b2
Paul Plotnikov <ppasha81@gmail.com>
2022-09-10 18:55:25 +0300

    Module

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,11 +1,8 @@
 package com;
 
-import com.config.FlywayConfig;
-import com.config.HibernateSessionFactoryUtil;
+
 import com.program.Program;
-import com.repository.group.GroupRepository;
-import com.service.GroupService;
-import org.flywaydb.core.Flyway;
+
 
 public class Main {
 
